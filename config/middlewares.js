796c2526cb9bdbc96ea32f467c6e1696f50ca535
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-07-02 23:37:02 +0530

    Update middleware.js to fix CORS issues

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,27 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'your-domain.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'your-domain.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173'], // Add your frontend URL here
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
