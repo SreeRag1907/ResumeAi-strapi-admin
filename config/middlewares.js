cdbebcb794cd14cea12b528e8e1652abf4e68c9c
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-07-03 10:31:44 +0530

    Update CORS and API request configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,11 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'your-domain.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'your-domain.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
-      origin: ['http://localhost:5173'], // Add your frontend URL here
+      origin: ['http://localhost:5173'],
     },
   },
   'strapi::poweredBy',
